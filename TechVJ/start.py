ca68c7c500613753bd62451218ddfd8491eb9202
streaam-developer <163761596+streaam-developer@users.noreply.github.com>
2026-02-16 13:16:31 +0530

    Update start.py

--- TechVJ/start.py
+++ TechVJ/start.py
@@ -172,7 +172,7 @@ async def save(client: Client, message: Message):
                             await client.send_message(message.chat.id, f"Error: {e}", reply_to_message_id=message.id)
 
             await acc.disconnect()
-            await asyncio.sleep(3)
+            await asyncio.sleep(10)
 
         batch_temp.IS_BATCH[message.from_user.id] = True
 
